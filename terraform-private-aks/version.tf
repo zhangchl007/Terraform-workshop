ea19d71f20f383ce316c74d16a985ac86f58ba4c
gitName <a@b.c>
2024-09-06 19:11:38 +0800

    add helm provider

--- terraform-private-aks/version.tf
+++ terraform-private-aks/version.tf
@@ -11,6 +11,10 @@ terraform {
       source  = "hashicorp/random"
       version = "~>3.0"
     }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.0"
+    }
   }
 
   backend "azurerm" {
